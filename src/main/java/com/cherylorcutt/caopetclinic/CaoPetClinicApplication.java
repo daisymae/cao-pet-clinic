2f5efc5b736058c121f30f81ec38d37d1513a916
daisymae <daisymae.orcadiafarm@gmail.com>
2019-09-25 12:33:39 -0500

    Initialized project
    Grabbed .gitignore and README from springframework guru. The rest of the files are generated by spring.io initialization of the project.

---
+++ src/main/java/com/cherylorcutt/caopetclinic/CaoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.cherylorcutt.caopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaoPetClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CaoPetClinicApplication.class, args);
+  }
+
+}
