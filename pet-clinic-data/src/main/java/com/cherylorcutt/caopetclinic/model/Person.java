4e8204e5f7ffd5dd9e200d5b36fe91a685bfa244
daisymae <daisymae.orcadiafarm@gmail.com>
2019-09-25 22:37:59 -0500

    adding base entity Closes #10.

--- pet-clinic-data/src/main/java/com/cherylorcutt/caopetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/cherylorcutt/caopetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.cherylorcutt.caopetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
   private String firstName;
   private String lastName;
--- pet-clinic-data/src/main/java/com/cherylorcutt/caopetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/cherylorcutt/caopetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.cherylorcutt.caopetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
   private String name;
 
